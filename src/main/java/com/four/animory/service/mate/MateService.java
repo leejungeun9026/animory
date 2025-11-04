777f14af4e814adaf24dc50f0bb3b294244353c4
dmsdl1126 <dmsdl1126@gmail.com>
2025-11-04 09:18:14 +0900

    feat 영랑님 mate파일 추가

---
+++ src/main/java/com/four/animory/service/mate/MateService.java
@@ -0,0 +1,9 @@
+package com.four.animory.service.mate;
+
+import com.four.animory.dto.mate.MateBoardDTO;
+
+import java.util.List;
+
+public interface MateService {
+    void regesterMateBoard(MateBoardDTO mateBoardDTO);
+}
---
+++ src/main/java/com/four/animory/service/mate/MateServiceImpl.java
@@ -0,0 +1,20 @@
+package com.four.animory.service.mate;
+
+
+import com.four.animory.dto.mate.MateBoardDTO;
+import com.four.animory.repository.mate.MateRepository;
+import com.four.animory.repository.user.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MateServiceImpl implements MateService {
+
+
+    @Override
+    public void regesterMateBoard(MateBoardDTO mateBoardDTO) {
+
+    }
+}
